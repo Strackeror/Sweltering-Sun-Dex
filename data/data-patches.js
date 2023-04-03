a215eb484dae2efc3cdbb337fb42872102515d7e
Strackeror <rremilavergne@gmail.com>
2023-04-03 11:03:10 +0200

    2023-04-03T11:03:10+02:00

--- data/data-patches.js
+++ data/data-patches.js
@@ -1,2 +1,11 @@
+// Hustle Typo
+BattleAbilities["hustle"].desc =
+  "Greatly boosts the Attack stat by 50% for a 20%\naccuracy drop on most Physical moves.";
 
-BattleAbilities["hustle"].desc = "Greatly boosts the Attack stat by 50% for a 20%\naccuracy drop on most Physical moves."
+// Fix PPBoosts for low pp moves
+for (let move_key in BattleMovedex) {
+  let move = BattleMovedex[move_key];
+  if (move.pp <= 3) {
+    move.noPPBoosts = true;
+  }
+}
